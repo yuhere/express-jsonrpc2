7bebe7906e508c4a3743fac6cea639ab94f39d54
yuhere@gmail.com <yuhere@gmail.com>
2016-12-26 21:00:06 +0800

    widened bodyParser size limitation to 1mb

--- src/index.js
+++ src/index.js
@@ -6,47 +6,6 @@ var path = require('path'),
   PropTypes = require('./PropTypes'),
   Repository = require('./Repository');
 
-var _json_parser = bodyParser.json({
-  verify: function (req, res, buf, encoding) {
-    req.rawBody = buf;
-  }
-});
-
-/**
- * Parse JSON from require.body.
- *
- * When Invalid JSON was received by the server or An error occurred on the server while parsing the JSON text.
- * SHOULD return JsonRpcError(-32700, "Parse error");
- *
- * @param req
- * @param res
- * @returns {Promise|Promise<T>}
- * @private
- */
-var _read_json = function (req, res) {
-  return new Promise(function (resolve, reject) {
-    try {
-      _json_parser(req, res, function (error) {  // override next() function
-        if (error) {
-          return reject({
-            id: null,
-            jsonrpc: utils.JSON_RPC_VERSION,
-            error: new JsonRpcError(-32700, "Parse error", error)
-          });
-        } else {
-          return resolve(req.body);
-        }
-      });
-    } catch (error) {
-      return reject({
-        jsonrpc: utils.JSON_RPC_VERSION,
-        error: new JsonRpcError(-32700, "Parse error", error)
-      })
-    }
-  });
-};
-
-//
 /**
  *
  * @param rpc_repository
@@ -54,6 +13,47 @@ var _read_json = function (req, res) {
  * @private
  */
 function _mk_rpc_dispatch(rpc_repository, mk_injectable, invoke_error_trans) {
+  //
+  var _json_parser = bodyParser.json({
+    limit: '1mb',
+    verify: function (req, res, buf, encoding) {
+      req.rawBody = buf;
+    }
+  });
+  /**
+   * Parse JSON from require.body.
+   *
+   * When Invalid JSON was received by the server or An error occurred on the server while parsing the JSON text.
+   * SHOULD return JsonRpcError(-32700, "Parse error");
+   *
+   * @param req
+   * @param res
+   * @returns {Promise|Promise<T>}
+   * @private
+   */
+  var _read_json = function (req, res) {
+    return new Promise(function (resolve, reject) {
+      try {
+        _json_parser(req, res, function (error) {  // override next() function
+          if (error) {
+            return reject({
+              id: null,
+              jsonrpc: utils.JSON_RPC_VERSION,
+              error: new JsonRpcError(-32700, "Parse error", error)
+            });
+          } else {
+            return resolve(req.body);
+          }
+        });
+      } catch (error) {
+        return reject({
+          jsonrpc: utils.JSON_RPC_VERSION,
+          error: new JsonRpcError(-32700, "Parse error", error)
+        })
+      }
+    });
+  };
+  //
   return function (req, res, next) {
     // console.log('_rpc_dispatch...', req.rpc);
     return _read_json(req, res).then(function (input) {
